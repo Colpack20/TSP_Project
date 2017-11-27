331634045820254b82b8cdb1a75df810541a8079
Colpack20 <Colpack20@gmail.com>
2017-11-26 21:35:48 -0500

    Update tspmain.cpp

--- tspmain.cpp
+++ tspmain.cpp
@@ -1,6 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int minKey(int key[], bool mstSet[], int n)
+{
+   // Initialize min value
+   int min = INT_MAX, min_index;
+ 
+   for (int v = 0; v < n; v++)
+     if (mstSet[v] == false && key[v] < min)
+         min = key[v], min_index = v;
+ 
+   return min_index;
+}
+int printMST(int parent[], int n, int **graph)
+{
+   printf("Edge   Weight\n");
+   for (int i = 1; i < n; i++)
+      printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
+}
+void primMST(int **graph, int size)
+{
+     int parent[size]; // Array to store constructed MST
+     int key[size];   // Key values used to pick minimum weight edge in cut
+     bool mstSet[size];  // To represent set of vertices not yet included in MST
+ 
+     // Initialize all keys as INFINITE
+     for (int i = 0; i < size; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+ 
+     // Always include first 1st vertex in MST.
+     key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
+     parent[0] = -1; // First node is always root of MST 
+ 
+     // The MST will have V vertices
+     for (int count = 0; count < size-1; count++)
+     {
+        // Pick the minimum key vertex from the set of vertices
+        // not yet included in MST
+        int u = minKey(key, mstSet, size);
+ 
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+ 
+        // Update key value and parent index of the adjacent vertices of
+        // the picked vertex. Consider only those vertices which are not yet
+        // included in MST
+        for (int v = 0; v < size; v++)
+ 
+           // graph[u][v] is non zero only for adjacent vertices of m
+           // mstSet[v] is false for vertices not yet included in MST
+           // Update the key only if graph[u][v] is smaller than key[v]
+          if (graph[u][v] && mstSet[v] == false && graph[u][v] <  key[v])
+             parent[v]  = u, key[v] = graph[u][v];
+     }
+ 
+     // print the constructed MST
+     printMST(parent, size, graph);
+}
 int main(int argc, char** argv)
 {
 	char buf[100];
@@ -61,5 +117,6 @@ int main(int argc, char** argv)
 	cout << endl;
 	}
 	infile.close();
+	primMST(ary, x); 
 	return 0;
 }
