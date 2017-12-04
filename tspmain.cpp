dd2683247ef7b0145e1be3f851e481bd41b192ea
Colpack20 <Colpack20@gmail.com>
2017-12-04 04:08:47 -0500

    Update tspmain.cpp

--- tspmain.cpp
+++ tspmain.cpp
@@ -149,20 +149,6 @@ int primMST(int **graph, int size)
 		mst[i][parent[i]] = graph[i][parent[i]];
 		mst[parent[i]][i] = graph[i][parent[i]];
 	}	
-	//perfect_matching(oddVertices);
-	//print mst 
-	/*for(int h = 0; h < size; h++)
-	{
-		for(int g = 0; g < size; g++)
-		{
-			cout << mst[h][g] << " ";
-		}
-		cout << endl;
-	}*/
-	//print odd vertices count
-	//for(int q = 0; q < size; q++)
-		//cout << "in oddVertices " << oddVertices[q] << endl;
-
 	//place in oddVertices array shows which vertex is odd or even
 	vector<int>oddV;
 	for(int z = 0; z < size; z++)
@@ -180,40 +166,7 @@ int primMST(int **graph, int size)
 		}
 		cout << "size is " << oddV.size() << endl;
 	}
-
-	//nearby matching
-	/*for (int i = (oddV.size()-1); i >= 0; i-=2){
-		mst[oddV[i]][oddV[i-1]] = graph[oddV[i]][oddV[i-1]];
-		mst[oddV[i-1]][oddV[i]] = graph[oddV[i]][oddV[i-1]];
-	}*/
-	//Updated matching algorithm
-	/*int shortest = 0;
-	int counterg = 0;
-	int neighborV;
-	//vector<int>match or int array[oddV.size()]
-	vector<int>match;
-	for (int i = 0; i < oddV.size(); i++)
-		for(int j = 0; j < oddV.size(); j++)
-			for(int k = 0; k < match.size(); k++)
-				if((match[k] != oddV[i] || match[k] != oddV[j]) && i != j){
-					if(counterg > 0)
-					{			
-						if(graph[oddV[i]][oddV[j]] < shortest){
-							shortest = graph[oddV[i]][oddV[j]];
-							neighborV = j;
-						}
-						if(j == oddV.size()-1){
-							match.push_back(i);
-							match.push_back(neighborV);
-						}
-					}
-					if(counterg == 0)
-					{	
-						shortest = graph[oddV[i]][oddV[j]];
-						counterg++;
-					}				
-				}*/
-	//Improved matching
+	//Matching algorithm
 	int shortest = 0;
 	int counterg = 0;
 	int neighborY, neighborX;
